a9b0e6a3dd9adb5a0214ff92376f5d9e29351dc0
50516004 <iwakura.kenshin.15@shizuoka.ac.jp>
2024-06-03 22:22:43 +0900

    円の色を変えれるように変更

--- src/p5/App.tsx
+++ src/p5/App.tsx
@@ -1,25 +1,43 @@
 import p5 from 'p5';
+import { useRef } from 'react';
 import useP5 from './useP5';
 
-function sketch(p: p5): void {
-    p.setup = function () {
-        p.createCanvas(800, 600);
-    };
+function sketch(p: p5, color: React.MutableRefObject<string>): void {
+  let x = 0;
+  let speed = 2;
 
-    p.draw = function () {
-        p.background(200);
-        p.fill(255, 0, 0);
-        p.ellipse(p.width / 2, p.height / 2, 100, 100);
-    };
+  p.setup = function() {
+    p.createCanvas(800, 600);
+  };
+
+  p.draw = function() {
+    p.background(200);
+    p.fill(p.color(color.current));
+    p.ellipse(x, p.height / 2, 100, 100);
+
+    x += speed;
+    if (x > p.width || x < 0) {
+      speed *= -1;
+    }
+  };
 }
 
-export default function App(): JSX.Element {
-    const canvasRef = useP5(sketch);
+function App(): JSX.Element {
+  const color = useRef<string>('red');
+  const canvasRef = useP5((p: p5) => sketch(p, color));
 
-    return (
-        <div>
-            <h1>My p5.js Sketch in React</h1>
-            <div ref={canvasRef}></div>
-        </div>
-    );
+  const changeColor = () => {
+    color.current = color.current === 'red' ? 'blue' : 'red';
+    console.log(color.current);
+  };
+
+  return (
+    <div>
+      <h1>My p5.js Sketch in React</h1>
+      <button onClick={changeColor}>Change Color</button>
+      <div ref={canvasRef}></div>
+    </div>
+  );
 }
+
+export default App;
