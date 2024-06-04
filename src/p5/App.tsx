6b1a353436e413b4f2b8cdac2efd2d5010ac53e0
50516004 <iwakura.kenshin.15@shizuoka.ac.jp>
2024-06-04 23:10:03 +0900

    コントローラ追加

--- src/p5/App.tsx
+++ src/p5/App.tsx
@@ -1,43 +1,77 @@
 import p5 from 'p5';
-import { useRef } from 'react';
+import { useState } from 'react';
 import useP5 from './useP5';
 
-function sketch(p: p5, color: React.MutableRefObject<string>): void {
-  let x = 0;
-  let speed = 2;
+const setting = {
+  background: "#282c34",
+  color: "#7fffd4",
+  speed: 2,
+};
 
-  p.setup = function() {
-    p.createCanvas(800, 600);
+function sketch(p: p5): void {
+  let x = 0;
+  let dir = 1;
+
+  p.setup = function () {
+    p.createCanvas(p.windowWidth - 5, p.windowHeight - 5);
   };
 
-  p.draw = function() {
-    p.background(200);
-    p.fill(p.color(color.current));
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth - 5, p.windowHeight - 5);
+  };
+
+  p.draw = function () {
+    p.background(setting.background);
+    p.fill(setting.color);
+    p.noStroke();
     p.ellipse(x, p.height / 2, 100, 100);
 
-    x += speed;
+    x += setting.speed * dir;
+
     if (x > p.width || x < 0) {
-      speed *= -1;
+      dir *= -1;
     }
   };
 }
 
-function App(): JSX.Element {
-  const color = useRef<string>('red');
-  const canvasRef = useP5((p: p5) => sketch(p, color));
+function ControlPanel() {
+  const [color, setColor] = useState(setting.color);
+  const [speed, setSpeed] = useState(setting.speed);
+  const [isDark, setIsDark] = useState(true);
 
-  const changeColor = () => {
-    color.current = color.current === 'red' ? 'blue' : 'red';
-    console.log(color.current);
-  };
+  return(
+    <div className='control-panel'>
+        <input
+          type="color"
+          value={color}
+          onChange={e => {
+            setting.color = (e.target.value);
+            setColor(setting.color);
+          }} />
+        <input
+          type='range'
+          value={speed}
+          min={1}
+          max={10}
+          onChange={e => {
+            setting.speed = parseInt(e.target.value);
+            setSpeed(setting.speed);
+          }} />
+          <button onClick={() => {
+            setting.background = isDark ? "white" : "#282c34";
+            setIsDark(!isDark);
+          }}>{isDark? "Light" : "Dark"}</button>
+      </div>
+  );
+}
+
+export default function App(): JSX.Element {
+  const canvasRef = useP5(sketch);
 
   return (
     <div>
-      <h1>My p5.js Sketch in React</h1>
-      <button onClick={changeColor}>Change Color</button>
+      <ControlPanel/>
       <div ref={canvasRef}></div>
     </div>
   );
 }
-
-export default App;
